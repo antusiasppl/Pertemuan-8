1f3bb3589f896f015b3bfa42cc228cfcbe84c15f
antusiasppl <ikhwanur30@gmail.com>
2024-05-30 15:00:13 +0700

    pertemuan 8

---
+++ vue-project-pertemuan-8/src/components/Artikel.vue
@@ -0,0 +1,5 @@
+<template>
+<h1>Aadalah komponen Artikel </h1>
+<p>Adalah Isi Artikel</p>
+<img src="https://w7.pngwing.com/pngs/866/905/png-transparent-university-of-amikom-yogyakarta-gadjah-mada-university-logo-organization-others-miscellaneous-purple-angle.png" alt="amikom">
+</template>
